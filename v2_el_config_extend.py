ac0a9b7f4cae2e899ed58959b223f6c57833e533
Roman Tyutin <60098816+Romanus190@users.noreply.github.com>
2025-08-13 09:51:51 +0300

    Add files via upload

--- v2_el_config_extend.py
+++ v2_el_config_extend.py
@@ -1,36 +1,39 @@
-def generate_electron_configuration_up_to_extended():
+import string
 
-    # Функция для генерации буквенного обозначения подуровня
+def generate_electron_configuration_up_to_extended():
     def get_sublevel_letter(l):
         standard_letters = ['s', 'p', 'd', 'f', 'g', 'h', 'i', 'k']
+
         if l < len(standard_letters):
             return standard_letters[l]
         else:
-            # Генерация букв для подуровней, выходящих за стандартные
-            # Например: l=8 -> 'l', l=9 -> 'm', и т.д.
-            return chr(ord('l') + l - 8)  # Начинаем с 'l', так как 'k' уже занята (l=7)
+            # Создаем бесконечную последовательность букв
+            letters = []
+            l -= 8  # Корректировка после 'k'
 
-    # Максимальное число электронов на подуровне (2*(2l + 1))
+            while l >= 0:
+                letters.append(string.ascii_lowercase[l % 26])
+                l = (l // 26) - 1
+
+            return ''.join(reversed(letters))
+
     def sublevel_max_e(l):
         return 2 * (2 * l + 1)
 
-    # Генерируем все возможные орбитали (n, l) по порядку заполнения
     configurations = []
     n = 1
-    while True:  # Бесконечный цикл для генерации всех возможных n
-        for l in range(0, n):  # l может быть от 0 до n-1
+
+    while True:
+        for l in range(0, n):
             configurations.append((n, l))
         n += 1
 
-        # Для демонстрации ограничим n, чтобы избежать бесконечного выполнения
-        # На практике можно убрать этот блок, если нужно действительно бесконечное количество
-        if n > 100:  # Пример ограничения для демонстрации
+        # Ограничение для демонстрации
+        if n > 1300:
             break
 
-    # Сортируем по правилу Клечковского (n + l, затем n)
     configurations.sort(key=lambda x: (x[0] + x[1], x[0]))
 
-    # Формируем последовательность
     result = []
     for n, l in configurations:
         sublevel_letter = get_sublevel_letter(l)
@@ -39,11 +42,13 @@ def generate_electron_configuration_up_to_extended():
 
     return result
 
-# Генерируем последовательность
+# Генерируем конфигурацию
 config = generate_electron_configuration_up_to_extended()
 
-print(' '.join(config[:100]))  # Выводим первые 100 элементов для демонстрации
+# Выводим последние 10 элементов для демонстрации
+for i in config:
+    print(i)
 
-# Записываем в файл
-with open('electron_configuration_extended.txt', 'w', encoding='utf-8') as f:
+# Сохраняем в файл
+with open('electron_config_extended.txt', 'w', encoding='utf-8') as f:
     f.write('\n'.join(config))
